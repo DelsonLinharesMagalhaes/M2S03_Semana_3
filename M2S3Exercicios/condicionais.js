9fa7611f937fa8242740845d74eed145e42735f0
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-05-22 11:51:37 -0300

    Exercicio4

--- M2S3Exercicios/condicionais.js
+++ M2S3Exercicios/condicionais.js
@@ -1,3 +1,5 @@
+// Exercicio 3 If e Else
+
 let salario = 4500
 let desconto = 0
 
@@ -11,4 +13,26 @@ if (salario < 2000){
     desconto = salario * 0.20
 }
 console. log("Desconto de "+desconto+" para o salário de "+salario+".")
+
+// Exercicio 4 Switch case
+
+let opcao = "2"
+switch (opcao){
+    case "1":
+        console.log("Opção 1")
+        break;
+    case "2":
+        console.log("Opção 2")
+        break;
+    case "3":
+        console.log("Opção 3")
+        break;
+    case "4":
+        console.log("Opção 4")
+        break;
+    default:
+        console.log("Opção 5")
+
+}
+
 
